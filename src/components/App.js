e4af072762de7736cbe86c1668929af04e53134f
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-10-29 12:13:21 +0200

    finish 4th task

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,15 @@ import data from '../data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from '../friends.json';
 
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
