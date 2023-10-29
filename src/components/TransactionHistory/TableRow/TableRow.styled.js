e247e1e20f9d3b426730dcf0a2d55244c05fa085
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-10-29 12:16:10 +0200

    add transition to hover table rows

--- src/components/TransactionHistory/TableRow/TableRow.styled.js
+++ src/components/TransactionHistory/TableRow/TableRow.styled.js
@@ -20,6 +20,7 @@ export const Currency = styled.td`
 export const Row = styled.tr`
   background-color: #ffffff;
   text-align: center;
+  transition: all 200ms ease;
 
   &:nth-child(even) {
     background-color: #ecf1f3;
@@ -27,5 +28,6 @@ export const Row = styled.tr`
 
   &:hover {
     background: linear-gradient(to bottom, #f2f9fe 0%, #d6f0fd 100%);
+    transform: scale(1.01);
   }
 `;
